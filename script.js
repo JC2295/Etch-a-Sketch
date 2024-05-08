15632efd913824360010432f864078ecd654dc1b
Jonathan Copeland <jonathan.copeland@hotmail.co.uk>
2024-05-08 15:43:50 +0100

    Created grid using JavaScript.

--- script.js
+++ script.js
@@ -1,1 +1,19 @@
+let gridContainer = document.querySelector("#gridContainer");
 
+for(let i = 0; i < 16; i++){
+
+    let gridRow = document.createElement("div");
+
+    gridRow.classList.add("row");
+
+    gridContainer.appendChild(gridRow);
+
+    for(let j = 0; j < 16; j++){
+        let gridCell = document.createElement("div");
+
+        gridCell.classList.add("cell");
+
+        gridRow.appendChild(gridCell);
+    }
+
+}

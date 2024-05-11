a82cc960fb171e71e7f654d922f67ede0e5c7de9
Jonathan Copeland <jonathan.copeland@hotmail.co.uk>
2024-05-11 12:22:52 +0100

    Added ui features

--- script.js
+++ script.js
@@ -1,32 +1,54 @@
-let gridContainer = document.querySelector("#gridContainer");
+let root = document.documentElement;
 
-for(let i = 0; i < 16; i++){
+let gridSizeInput = document.querySelector("#gridSizeInput");
 
-    let gridRow = document.createElement("div");
+let gridSizeValue = gridSizeInput.value;
 
-    gridRow.classList.add("row");
+gridSizeInput.addEventListener("change", updateGridSize);
 
-    gridContainer.appendChild(gridRow);
+function updateGridSize(){
 
-    for(let j = 0; j < 16; j++){
-        let gridCell = document.createElement("div");
+    gridSizeValue = gridSizeInput.value;
 
-        gridCell.classList.add("cell");
+    document.querySelector("GridContainer")
 
-        gridRow.appendChild(gridCell);
+    let gridContainer = document.querySelector("#gridContainer").innerHTML = "";
 
-        gridCell.addEventListener("mousedown", shade);
+    createGrid();
 
-        gridCell.addEventListener("mouseover", shade)
+}
+
+function createGrid(){
+
+    for(let i = 0; i < gridSizeValue; i++){
+
+        let gridRow = document.createElement("div");
+
+        gridRow.classList.add("row");
+
+        gridContainer.appendChild(gridRow);
+
+        for(let j = 0; j < gridSizeValue; j++){
+            let gridCell = document.createElement("div");
+
+            gridCell.classList.add("cell");
+
+            gridRow.appendChild(gridCell);
+
+            gridCell.addEventListener("mousedown", shade);
+
+            gridCell.addEventListener("mouseover", shade)
+        }
+
     }
 
-}
 
+    function shade(){
+        this.classList.add("shade");
+    }
 
-function shade(){
-    this.classList.add("shade");
-}
+    function  unshade(){
+        this.classList.remove("shade");
+    }
 
-function  unshade(){
-    this.classList.remove("shade");
-}
+};
